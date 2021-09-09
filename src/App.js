d9ea89fcbc4f6111cd1da31c00f888405298b019
Thara Supasiti <sapiensis@gmail.com>
2021-09-09 12:08:57 +1000

    set up linting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl text-gray-600"> Hello world </h1>
+      <h1 className="text-3xl text-gray-600 text-center"> Hello world </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
