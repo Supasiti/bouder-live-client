beda1e98a9b6f562e119a915288382d3a08ec0f3
Thara Supasiti <sapiensis@gmail.com>
2021-09-14 11:51:47 +1000

    format homepage

--- src/home.js
+++ src/home.js
@@ -7,16 +7,16 @@ const Home = () => (
   <div>
     <Navbar height="h-12 sm:h-16" />
     <main>
-      <Container extraClasses="p-4 pt-16 sm:pt-20">
-        <div className="flex flex-wrap justify-center">
-          <div className="max-w-sm md:w-1/2">
+      <Container extraClasses="p-4 pt-16 sm:pt-20 md:pt-24">
+        <div className="flex flex-wrap justify-center ">
+          <div className="max-w-sm sm:py-10 md:w-1/2  md:px-6 md:py-12 lg:max-w-md">
             <Hero />
           </div>
 
           <div
             className="w-full mt-8 
               sm:max-w-md sm:mx-auto
-              md:w-1/2 md:mt-12 md:max-w-xl lg:max-w-4xl"
+              md:w-1/2   md:mx-0 md:mt-12 md:max-w-xl lg:max-w-4xl"
           >
             <LoginCard />
           </div>
